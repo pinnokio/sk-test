23134de8e13bb7733127c00ecc90ee6d69c127ef
Konstantin Lazarenko <pinnokio@gmail.com>
2013-12-08 14:42:32 +0200

    changed timezone settings, added faculty app

--- students/students/settings.py
+++ students/students/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'faculty',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
